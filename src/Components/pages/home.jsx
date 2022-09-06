21253c8bc85fa74087f625119f119a72425798be
TweakyTurnip875 <44481502+TweakyTurnip875@users.noreply.github.com>
2022-09-06 18:17:02 +0000

    Homepage styles

--- src/Components/pages/home.jsx
+++ src/Components/pages/home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
-
-
 const Home = () => {
   return(
-    <div>
-      <h1>Home</h1>
+    <div className="home-container">
+      <div className="home-wrapper">
+        <div className="home-background-container">
+          <div className="class-school-wrapper">
+            <h1>Launch High School</h1>
+            <h2>Class of 2023</h2>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

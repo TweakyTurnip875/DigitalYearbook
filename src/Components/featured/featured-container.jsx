a72f08727a7716392c8450bebf12d3ab6e02fdb2
TweakyTurnip875 <44481502+TweakyTurnip875@users.noreply.github.com>
2022-09-26 15:23:26 +0000

    darker theme

--- src/Components/featured/featured-container.jsx
+++ src/Components/featured/featured-container.jsx
@@ -1,12 +1,15 @@
 import { Component } from 'react'
 import { Dropbox } from 'dropbox';
+import axios from 'axios'
+//import crypto from 'crypto'
+
 
 import FeaturedItem from './featured-item'
 
-const accessToken = import.meta.env.VITE_KEY + "";
+const accessToken = "sl.BQASn4GeJUPVm41nGwntP2OSfu4ir4YFB3miuk0NZCWjhSwl8mxRQLjktSF6O5JUTJjkSEpl2R4e7pIDpGrJG0LQ5To5R_-g30YFKwgqFBx2SC6ZcFBDiCsWbucwGlFdI1o5eurm25SH";
 
 export default class FeaturedContainer extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
 
     this.state = {
@@ -14,22 +17,27 @@ export default class FeaturedContainer extends Component {
     }
     this.getItems = this.getItems.bind(this)
     this.handleSetFeaturedItem = this.handleSetFeaturedItem.bind(this)
+    this.getAccessToken = this.getAccessToken.bind(this)
   }
-  
+
+  getAccessToken() {
+
+  }
+
   getItems() {
     const dbx = new Dropbox({
       accessToken,
-      
-    })
-      dbx.sharingGetSharedLinks({
-        path: ''
-      }).then(res => {
-        console.log(res)
-        this.setState({
-          data: res.result.links.map(i => i.url + "&raw=1 ")
-        })
 
+    })
+    dbx.sharingGetSharedLinks({
+      path: ''
+    }).then(res => {
+      console.log(res)
+      this.setState({
+        data: res.result.links.map(i => i.url + "&raw=1 ")
       })
+
+    })
 
   }
   handleSetFeaturedItem() {
@@ -41,8 +49,8 @@ export default class FeaturedContainer extends Component {
     this.getItems()
   }
   render() {
-    return(
-      
+    return (
+
       <div>
         <div className="featured-wrapper">{this.handleSetFeaturedItem()}</div>
       </div>
